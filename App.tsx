a79f6e0bdb8a91d16ba73022a4c66372391d76ad
italo Vinicius <italorodrigues476@icloud.com>
2024-04-14 13:36:52 +0100

    criando um app on-off

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
 
+// integrando as funçoes
 export default function App() {
+  const [isActive, setisActive] = useState (false)
+  const [ligado, funcaoLigar] = useState(false)
+
+// nomeando as funçoes
+  function handleSymbol(){
+
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+    console.log('Bem-vindo Italo')
+  }
+  // posso adicionar textos dentro da tag VIEW
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={isActive ? styles.containerOn : 
+      styles.containerOff
+    }>
+      <TouchableOpacity onPress={handleSymbol}>
+      <Image source={isActive ? symbolOn : symbolOff}/>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#48dbfb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerOff:{
+    flex: 1,
+    backgroundColor:'#ff9f43',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
